6e55f5edd70e03cba483c021eeee2b9bcca1ce4f
Abel Sintaro <abelofficial94@gmail.com>
2022-06-21 12:23:31 +0200

    Added test for login user controller.

--- Backend/API.Tests/ControllersTest.cs
+++ Backend/API.Tests/ControllersTest.cs
@@ -19,6 +19,7 @@ public class ControllersTest
     private void setupMock()
     {
         _mock.Setup(m => m.Send(It.IsAny<RegisterUserCommand>(), default(CancellationToken))).ReturnsAsync(new UserResponseDto());
+        _mock.Setup(m => m.Send(It.IsAny<LoginUserCommand>(), default(CancellationToken))).ReturnsAsync(new LoginResponseDto());
     }
 
     [Fact]
@@ -32,5 +33,18 @@ public class ControllersTest
 
         // Assert
         _mock.Verify(x => x.Send(It.IsAny<RegisterUserCommand>(), default(CancellationToken)), Times.Once());
+    }
+
+    [Fact]
+    public async void login_user_should_call_mediator_handler()
+    {
+        // Arrange
+        var controller = new AuthController(It.IsAny<ILogger<AuthController>>(), _mock.Object);
+
+        // Act
+        await controller.LoginUser(It.IsAny<LoginUserCommand>());
+
+        // Assert
+        _mock.Verify(x => x.Send(It.IsAny<LoginUserCommand>(), default(CancellationToken)), Times.Once());
     }
 }
