e6c2ab1a6b4da12afb30b94cc4246079b520d3d0
Abel Sintaro <abelofficial94@gmail.com>
2022-06-24 10:04:42 +0200

    Fixed broken tests.

---
+++ Backend/API.Tests/SqliteInMemory.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Data.Common;
+using API.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Api.Tests;
+public class SqliteInMemory : BaseHandlerTest, IDisposable
+{
+    private readonly DbConnection _connection;
+
+    public SqliteInMemory()
+      : base(
+          new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlite(CreateInMemoryDatabase())
+            .Options)
+    {
+        _connection = RelationalOptionsExtension.Extract(ContextOptions).Connection;
+    }
+    private static DbConnection CreateInMemoryDatabase()
+    {
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        return connection;
+    }
+    public void Dispose() => _connection.Dispose();
+}
--- Backend/API.Tests/commandsTest/LoginUserTest.cs
+++ Backend/API.Tests/commandsTest/LoginUserTest.cs
@@ -1,77 +1,30 @@
-using System.Linq.Expressions;
 using System.Net;
 using API.Application.Commands;
-using API.Application.Profiles;
-using API.Application.Results;
-using API.Data.Entities;
-using API.Data.ValueObjects;
-using AutoMapper;
+using API.Data;
 using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using Moq;
 
 namespace Api.Tests.CommandsTest;
 
-public class LoginUserTest
+public class LoginUserTest : SqliteInMemory
 {
-    private readonly Mock<IRepository<User>> _repoMock;
+
     private readonly Mock<IConfiguration> _configMock;
-    private readonly IMapper _mapper;
+    private readonly AppDbContext _context;
 
     public LoginUserTest()
     {
-
-        _repoMock = new Mock<IRepository<User>>();
         _configMock = new Mock<IConfiguration>();
-
-        var mapperConfig = new MapperConfiguration(c =>
-        {
-            c.AddProfile<CommandToEntity>();
-            c.AddProfile<EntityToResult>();
-        });
-        _mapper = mapperConfig.CreateMapper();
-
-        _configMock.SetupGet(x => x[It.Is<string>(s => s == "JWT:secret")]).Returns("test-jwt-token-3.k{efCnVT5)}mw");
-    }
-
-    private void setupMockHappy()
-    {
-        var password = new Password("TestPassword");
-
-        _repoMock.Setup(c => c.GetAll(It.Is<Expression<Func<User, bool>>>(u => true)))
-        .ReturnsAsync(() => new List<User> { new User() { Id = 0, Username = "test-user", PasswordHash = password.PasswordHash, PasswordSalt = password.PasswordSalt } });
-    }
-
-    private void setupMockSad()
-    {
-        _repoMock.Setup(c => c.GetAll(It.Is<Expression<Func<User, bool>>>(u => true)))
-        .ReturnsAsync(() => new List<User> { });
-    }
-
-    [Fact]
-    public async void login_user_handler_should_return_resultDto_on_success()
-    {
-        // Arrange
-        setupMockHappy();
-        var handler = new LoginUserHandler(_mapper, _repoMock.Object, _configMock.Object);
-        var command = new LoginUserCommand()
-        {
-            Password = "TestPassword"
-        };
-
-        // Act
-        var result = await handler.Handle(command, CancellationToken.None);
-
-        // Assert
-        result.Should().BeOfType<LoginResponseDto>();
+        _context = new AppDbContext(ContextOptions);
     }
 
     [Fact]
     public async void login_user_handler_should_throw_unauthorized_on_user_not_found()
     {
         // Arrange
-        setupMockSad();
-        var handler = new LoginUserHandler(_mapper, _repoMock.Object, _configMock.Object);
+
+        var handler = new LoginUserHandler(_mapper, _context, _configMock.Object);
         var command = new LoginUserCommand()
         { };
 
@@ -86,8 +39,8 @@ public class LoginUserTest
     public async void login_user_handler_should_throw_unauthorized_on_wrong_password()
     {
         // Arrange
-        setupMockHappy();
-        var handler = new LoginUserHandler(_mapper, _repoMock.Object, _configMock.Object);
+
+        var handler = new LoginUserHandler(_mapper, _context, _configMock.Object);
         var command = new LoginUserCommand()
         {
             Username = "TestUser",
