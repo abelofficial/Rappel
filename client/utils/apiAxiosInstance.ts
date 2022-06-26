4544bdec308e34ecb458878d502b3a7c6b7d371f
Abel Sintaro <abelofficial94@gmail.com>
2022-06-26 07:23:50 +0200

    Hot fix (#16)
    * Bug fix.
    
    * Fix backend and frontend communication issue.

--- client/utils/apiAxiosInstance.ts
+++ client/utils/apiAxiosInstance.ts
@@ -4,7 +4,7 @@ export const api = (token?: string) =>
   axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND,
     headers: {
-      Authorization: `bearer ${token?.slice(1, token.length - 1)}`,
+      Authorization: `bearer ${token}`,
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
     },
