1641e383940cc833b7082ced90f08e2386cb20ed
Abel Sintaro <abelofficial94@gmail.com>
2022-06-29 17:32:20 +0200

    Updated git action to work on push for dev branch.

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -5,21 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.InstallServicesFromAssembly(builder.Configuration);
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseRouting();
-
 app.UseCors();
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
